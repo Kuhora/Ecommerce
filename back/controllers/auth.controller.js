b6e9e79e6f704c6013a4c33e81cc8b42b0a9af37
kuhora <kuhora123@gmail.com>
2025-08-06 14:15:30 -0300

    Token check

--- back/controllers/auth.controller.js
+++ back/controllers/auth.controller.js
@@ -99,3 +99,28 @@ export const logout = async (req, res) => {
         res.status(500).json({ message: "Server error", error: error.message });
     }
 }
+
+export const refreshToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if(!refreshToken) {
+            return res.status(401).json({ message: "No refresh token provided"});
+        }
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+        if(storedToken !== refreshToken){
+            return res.status(401).json({ message: "Invalid refresh token"});
+        }
+
+        const acessToken = jwt.sign({ userId: decoded.userid }, process.env.ACESS_TOKEN_SECRET, {expiresIn: "15m"});
+        res.cookie("acessToken", acessToken, {httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: "strict", maxAge: 15 * 60 * 1000 });
+    
+        res.json({message:"Token refresh successfully"});
+    } catch (error) {
+        console.log("Error in refreshToken controller", error.message);
+        res.status(500).json({message:"Server error", error: error.message});
+    }
+}
